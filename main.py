32d3fd621f52449db9766126594bc1be9afcdc55
edmond120 <edmondwong888@msn.com>
2018-02-21 23:20:11 -0500

    ident function done

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from matrix import *
 screen = new_screen()
 color = [ 255, 0, 0 ]
 matrix = new_matrix()
-
-draw_lines( matrix, screen, color )
-display(screen)
-save_extension(screen, 'img.png')
+print_matrix( matrix )
+print "----"
+print_matrix( ident( matrix ) )
+#draw_lines( matrix, screen, color )
+#display( screen )
+#save_extension( screen, 'img.png' )
--- matrix.py
+++ matrix.py
@@ -2,16 +2,26 @@ import math
 
 
 def print_matrix( matrix ):
-    pass
+	rows = len(matrix[0])
+	for row in range( rows ):
+		print '[ ',
+		for col in matrix:
+			p = str(col[row])
+			if len(p) < 3:
+				p = ' ' * (3 - len(p)) + p
+			print p + ' ',
+		print ']\n',
 
 def ident( matrix ):
-    pass
+	l = len(matrix[0])
+	m = new_matrix(rows=l,cols=l)
+	for i in range( l ):
+		m[i][i] = 1
+	return m
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
     pass
-
-
 
 
 def new_matrix(rows = 4, cols = 4):
