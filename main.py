0e8c84900f4ef204c31baf8e17309b555d7b0eb4
edmond120 <edmondwong888@msn.com>
2018-02-21 23:21:49 -0500

    print statement change

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ from matrix import *
 screen = new_screen()
 color = [ 255, 0, 0 ]
 matrix = new_matrix()
+print "\nmatrix = new_matrix"
 print_matrix( matrix )
-print "----"
+print "\nident( matrix )"
 print_matrix( ident( matrix ) )
 #draw_lines( matrix, screen, color )
 #display( screen )
