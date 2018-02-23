b1e374db9c335a0257a76c788b0541993a7d0699
edmond120 <edmondwong888@msn.com>
2018-02-23 00:14:26 -0500

    finished with matrix mult

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from matrix import *
 
 screen = new_screen()
 color = [ 255, 0, 0 ]
-matrix = new_matrix()
-print "\nmatrix = new_matrix"
+matrix = [[1,2,3],[4,5,6]]
+print "\nmatrix"
 print_matrix( matrix )
 print "\nident( matrix )"
 print_matrix( ident( matrix ) )
+print "\nother_matrix"
+other_matrix = [[7,8],[9,10],[11,12]]
+print_matrix( other_matrix )
+print "\nmatrix_mult(matrix,other_matrix)"
+print_matrix( matrix_mult( matrix, other_matrix ) )
 #draw_lines( matrix, screen, color )
 #display( screen )
 #save_extension( screen, 'img.png' )
--- matrix.py
+++ matrix.py
@@ -2,15 +2,14 @@ import math
 
 
 def print_matrix( matrix ):
-	rows = len(matrix[0])
-	for row in range( rows ):
+	rows = len(matrix)
+	cols = len(matrix[0])
+	for row in range(rows):
 		print '[ ',
-		for col in matrix:
-			p = str(col[row])
-			if len(p) < 3:
-				p = ' ' * (3 - len(p)) + p
-			print p + ' ',
-		print ']\n',
+		for col in range(cols):
+			s = str(matrix[row][col])
+			print ' ' * (3 - len(s)) + s + ' ',
+		print ']'
 
 def ident( matrix ):
 	l = len(matrix[0])
@@ -21,13 +20,24 @@ def ident( matrix ):
 
 #m1 * m2 -> m2
 def matrix_mult( m1, m2 ):
-    pass
+	if(len(m1[0]) == len(m2)):
+		rows = len(m1)
+		cols = len(m2[0])
+		m = new_matrix(rows,cols)
+		for row in range(len(m1)):
+			m2c = range(len(m2[0]))
+			for i in range(len(m1[0])):
+				for col in m2c:
+					m[row][col] += m1[row][i] * m2[i][col]
+		return m
+	else:
+		return None
 
 
 def new_matrix(rows = 4, cols = 4):
     m = []
-    for c in range( cols ):
+    for r in range( rows ):
         m.append( [] )
-        for r in range( rows ):
-            m[c].append( 0 )
+        for c in range( cols ):
+            m[r].append( 0 )
     return m
